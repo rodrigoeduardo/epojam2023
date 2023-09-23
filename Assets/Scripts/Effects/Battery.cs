baacfcefd5971b76b32053d4889b68c233806674
KievL <kiev.sider@hotmail.com>
2023-09-23 19:35:45 -0300

    cell and battery

--- Assets/Scripts/Effects/Battery.cs
+++ Assets/Scripts/Effects/Battery.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
+
 
 public class Battery : ItemsEffects
 {
-    public Light2D test;
     public override void RunEffect()
     {
         // Aumenta o raio de luz
-        test = this.GetComponent<Light2D>();
-        test.pointLightOuterRadius = test.pointLightOuterRadius*1.5f;
+        UnityEngine.Rendering.Universal.Light2D test = light.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+        test.pointLightOuterRadius = test.pointLightOuterRadius*2f;
 
 
         // Implemente o código para aumentar o raio de luz aqui
--- Assets/Scripts/Effects/Cellphone.cs
+++ Assets/Scripts/Effects/Cellphone.cs
@@ -1,10 +1,25 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cellphone : ItemsEffects
 {
     public override void RunEffect()
-    {
+    {        
         // Liga para a filha e mostra sua localização no celular
         // Implemente o código para ligar para a filha e mostrar a localização aqui
+        GameObject cell = this.gameObject;
+        cell.SetActive(true);
+        StartCoroutine(phoneRing(cell));
+        
+    }
+
+    IEnumerator phoneRing(GameObject cell){
+        //COMEÇAR SOM
+        
+        yield return new WaitForSeconds(15f);
+        cell.SetActive(false);
+        //PARAR SOM
+
     }
 }
