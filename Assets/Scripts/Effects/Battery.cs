d23c9a4603bbd74d2e8b654b9c11497b74014b57
Mateus Oliveira <devmateusalves@gmail.com>
2023-09-24 00:10:10 -0300

    Prefabs items effects

--- Assets/Scripts/Effects/Battery.cs
+++ Assets/Scripts/Effects/Battery.cs
@@ -1,15 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
 
 public class Battery : ItemsEffects
 {
+
     public override void RunEffect()
     {
         // Aumenta o raio de luz
         UnityEngine.Rendering.Universal.Light2D test = light.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
         test.pointLightOuterRadius = test.pointLightOuterRadius*2f;
-
-
-        // Implemente o código para aumentar o raio de luz aqui
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/Effects/Beer.cs
+++ Assets/Scripts/Effects/Beer.cs
@@ -1,8 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 public class Beer : ItemsEffects
 {
+
     public override void RunEffect()
     {
         // Pisca a luz
